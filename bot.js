83726ea429aa884aca32f6cf684bd9e900b50a8f
DavidCzPdy1 <52082894+DavidCzPdy1@users.noreply.github.com>
2020-03-13 23:16:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ function msToTime(duration) {
   minutes = (minutes < 10) ? "0" + minutes : minutes;
   seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-  return (hours * 60 + minutes) > 5 ? hours + "h " + minutes + "m " : "SOON" ;
+  return (hours * 60 + minutes) > 30 ? hours + "h " + minutes + "m " : "SOON" ;
 }
 
 function RemaningTime(now) {
