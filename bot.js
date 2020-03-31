ad68b9063785ab43e32d60799f8ca7d03b346db6
DavidCzPdy1 <52082894+DavidCzPdy1@users.noreply.github.com>
2020-03-31 21:27:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -96,7 +96,7 @@ var rolename=["Member","Notice"];
 
 
 client.on('message', msg => {
-	if(msg.member.hasPermission('MANAGE_ROLES')) {
+	// if(msg.member.hasPermission('MANAGE_ROLES')) {
 		if(msg.content.startsWith("!reaction")){
 			if(!msg.channel.guild) return;
 				for(let n in emojiname){
@@ -107,7 +107,7 @@ client.on('message', msg => {
 				}
 			}
 		}	
-	}
+	// }
 });
 
 client.on("messageReactionAdd",(reaction,user)=>{
