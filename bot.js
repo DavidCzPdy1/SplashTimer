0b3a23f44e62da2fb6968c03ed5dda6c80abd9c5
DavidCzPdy1 <52082894+DavidCzPdy1@users.noreply.github.com>
2020-04-09 11:13:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,9 +30,17 @@ function RemaningTime(now) {
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-lient.on("ready", () =>{
+client.on("ready", () =>{
     console.log(`Logged in as ${client.user.tag}!`);
-   
+    client.user.setPresence({
+        status: "online",  //You can show online, idle....
+        game: {
+            name: "nice stories with Farmans",  //The message shown
+            type: "LISTENING" //PLAYING: WATCHING: LISTENING: STREAMING:
+        }
+    });
+ });
+
 
 const splashDates = {
 'Sun': 12,
